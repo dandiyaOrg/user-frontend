de52d2504b33f23fd8a284f6bba5bf922c01d841
Kunal <kunaljangid1245@gmail.com>
2025-09-21 00:28:17 +0530

    Footer Update new

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,10 +8,10 @@ const Footer = () => {
     <footer className="bg-gray-900 text-white">
       {/* Main Footer Content */}
       <div className="max-w-7xl mx-auto px-4 py-16">
-        <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-32">
+        <div className="flex flex-col md:flex-row md:items-center justify-between gap-3 md:gap-4">
           
           {/* Brand Section */}
-          <div className="lg:col-span-1">
+          <div className="w-full md:w-1/2 lg:w-1/3 flex flex-col  justify-between">
             <div className="flex items-center space-x-3 mb-6">
               <div className="w-10 h-10 bg-gradient-to-r from-blue-500 to-purple-600 rounded-xl flex items-center justify-center">
                 <span className="text-white font-bold text-lg">L</span>
